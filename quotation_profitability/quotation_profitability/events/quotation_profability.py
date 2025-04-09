abe73c6a78eff18bd50d8d67327b07c5d49aec13
SufyanSadiqOffice <sufyan.sadiq@sowaan.com>
2025-04-09 10:56:40 +0500

    Making corrections on Quotation Item for all type of custom rates and costs.

--- quotation_profitability/quotation_profitability/events/quotation_profability.py
+++ quotation_profitability/quotation_profitability/events/quotation_profability.py
@@ -11,30 +11,42 @@ def profability(doc,method):
     for i in doc.items:
         # Check if i.base_rate is not zero before performing the division
         if i.base_rate != 0:
-            i.custom_clearance = i.custom_clearance * (i.base_rate / 100) * i.qty
-            i.custom_freight_charges = i.custom_freight_charges * (i.base_rate / 100) * i.qty
-            i.custom_installation_charges = i.custom_installation_charges * i.qty
-            i.custom_customs = i.custom_customs * (i.base_rate / 100) * i.qty
-            i.custom_item_cogs = i.custom_item_cogs * i.qty
+            i.custom_clearance_cost = (i.custom_clearance or 0) * (i.base_rate / 100) * i.qty
+            i.custom_freight_charges_cost = (i.custom_freight_charges or 0) * (i.base_rate / 100) * i.qty
+            i.custom_installation_charges_cost = (i.custom_installation_charges or 0) * i.qty
+            i.custom_customs_cost = (i.custom_customs or 0) * (i.base_rate / 100) * i.qty
+
+
+
+
+            i.custom_item_cogs_cost = (i.custom_item_cogs or 0) * i.qty
+
+
+
             i.custom_total_landed_cost = (
-                i.custom_clearance+ i.custom_freight_charges + i.custom_installation_charges+ i.custom_customs
+                i.custom_clearance_cost + i.custom_freight_charges_cost + i.custom_installation_charges_cost + i.custom_customs_cost
             )
-            i.custom_total_costs = i.custom_total_landed_cost + i.custom_item_cogs
+            i.custom_total_costs = i.custom_total_landed_cost + i.custom_item_cogs_cost
 
             # Accumulate the item cost to the totalcost
-            totalcost = totalcost + i.custom_item_cogs
+            totalcost = totalcost + i.custom_item_cogs_cost
 
             # Accumulate the additional costs to totaladditionalcost
-            totaladditionalcost = totaladditionalcost + i.custom_total_landed_cost 
+            totaladditionalcost = totaladditionalcost + i.custom_total_landed_cost
+
+
+
 
     # Assign the totalcost to doc.custom_total_cost
     doc.custom_total_item_cogs = totalcost
+    doc.custom_total_cogs = totalcost
 
     # Assuming doc.base_total is available, assign it to doc.custom_total_amount
     doc.custom_total_selling_volume = doc.base_total
 
     # Assign the totaladditionalcost to doc.custom_additional_costs
     doc.custom_additional_costs = totaladditionalcost
+    doc.custom_total_dry_cost = totaladditionalcost
 
     doc.custom_gross_profit = doc.custom_total_selling_volume - doc.custom_total_item_cogs 
     doc.custom_gross_profit_percentage = doc.custom_gross_profit * 100 / doc.custom_total_selling_volume
