712a531b2739285f397be06114d1eca78323118d
SufyanSadiqOffice <sufyan.sadiq@sowaan.com>
2025-04-15 16:11:17 +0500

    Update app for costing rates in BQO.

--- quotation_profitability/quotation_profitability/events/quotation_profability.py
+++ quotation_profitability/quotation_profitability/events/quotation_profability.py
@@ -3,36 +3,42 @@ import frappe
 
 def profability(doc,method):
 
-    # Initialize totalcost and totaladditionalcost outside the loop
+
+    check_box_value = frappe.db.get_single_value('Selling Settings', 'custom_costing_rate_from_product_bundle')
+    if not check_box_value :
+        check_box_value = 0
+
     totalcost = 0
     totaladditionalcost = 0
-
-    # Iterate over items in doc.items
+    
     for i in doc.items:
-        # Check if i.base_rate is not zero before performing the division
-        if i.base_rate != 0:
-            i.custom_clearance_cost = (i.custom_clearance or 0) * (i.base_rate / 100) * i.qty
-            i.custom_freight_charges_cost = (i.custom_freight_charges or 0) * (i.base_rate / 100) * i.qty
-            i.custom_installation_charges_cost = (i.custom_installation_charges or 0) * i.qty
-            i.custom_customs_cost = (i.custom_customs or 0) * (i.base_rate / 100) * i.qty
+        sub_cost = 0
+        sig = 0
+        
+        i.custom_dry_cost_rate_calculates_from_product_bundle_amounts = 0
+        i.custom_item_cogs_cost = (i.custom_item_cogs or 0) * i.qty
+        i.custom_total_landed_per_unit_cost = ( (i.custom_clearance_cost or 0) + (i.custom_freight_charges_cost or 0) + (i.custom_installation_charges_cost or 0) + (i.custom_customs_cost or 0) )
+        
+        if check_box_value == 1 :
+
+            if doc.packed_items :
+                for j in doc.packed_items :
+                    if i.item_code == j.parent_item :
+                        sub_cost = sub_cost + ( (j.custom_costing_rate or 0) * (j.qty or 0) )
+                        sig = 1
+            if sig == 1 :
+                i.custom_total_landed_per_unit_cost = sub_cost - (i.custom_item_cogs or 0)
+                i.custom_dry_cost_rate_calculates_from_product_bundle_amounts = 1
+        
+        
+        i.custom_total_landed_cost = i.custom_total_landed_per_unit_cost * i.qty
+        i.custom_total_costs = i.custom_total_landed_cost + i.custom_item_cogs_cost
+
+        totalcost = totalcost + i.custom_item_cogs_cost
+        totaladditionalcost = totaladditionalcost + i.custom_total_landed_cost
 
 
 
-
-            i.custom_item_cogs_cost = (i.custom_item_cogs or 0) * i.qty
-
-
-
-            i.custom_total_landed_cost = (
-                i.custom_clearance_cost + i.custom_freight_charges_cost + i.custom_installation_charges_cost + i.custom_customs_cost
-            )
-            i.custom_total_costs = i.custom_total_landed_cost + i.custom_item_cogs_cost
-
-            # Accumulate the item cost to the totalcost
-            totalcost = totalcost + i.custom_item_cogs_cost
-
-            # Accumulate the additional costs to totaladditionalcost
-            totaladditionalcost = totaladditionalcost + i.custom_total_landed_cost
 
 
 
@@ -52,3 +58,5 @@ def profability(doc,method):
     doc.custom_gross_profit_percentage = doc.custom_gross_profit * 100 / doc.custom_total_selling_volume
     doc.custom_net_profit = doc.custom_gross_profit- doc.custom_additional_costs
     doc.custom_net_profit_percentage = doc.custom_net_profit * 100 / doc.custom_total_selling_volume
+
+    doc.custom_grand_cost = doc.custom_total_dry_cost + doc.custom_total_cogs
